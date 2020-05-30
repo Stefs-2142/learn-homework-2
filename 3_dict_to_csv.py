8887b3bd4aeecf38e869f9c25867d43ebed1ab20
Stepan Bubnov <exectrogod@gmail.com>
2020-05-30 16:18:13 +0300

    Выполнил задание.

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -1,3 +1,5 @@
+import csv
+
 """
 Домашнее задание №2
 
@@ -7,13 +9,28 @@
 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
+user_data = [{'name': 'Stepan', 'age': 24, 'job': 'Python developer'}, {'name': 'Igor', 'age': 36, 'job': 'Lawyer'},
+            {'name': 'Svetlana', 'age': 22, 'job': 'Dentist'}, {'name': 'Ali', 'age': 54, 'job': 'Vet'}]
+
+
+def get_csv(data):
+    """ Converting user dict to .cvs file """
+    
+    with open('people_info.cvs', 'w', encoding='utf-8') as f:
+        fields = ['name', 'age', 'job']
+        writer = csv.DictWriter(f, fields, delimiter=':')
+        writer.writeheader()
+        for people in data:
+            writer.writerow(people)
+
+
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    get_csv(user_data)
 
 if __name__ == "__main__":
     main()
