6aa26f17ee0e24f83d6830bc60bf28e7ba6ba6c6
Stepan Bubnov <exectrogod@gmail.com>
2020-06-13 18:10:09 +0300

    4/5 done

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -79,17 +79,33 @@ def count_3():
 
 # Задание 4
 # Для каждого класса нужно вывести количество девочек и мальчиков в нём.
-school = [
-  {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-  {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-]
-is_male = {
-  'Маша': False,
-  'Оля': False,
-  'Олег': True,
-  'Миша': True,
-}
-# ???
+def count_4():
+
+	school = [
+  	{'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
+  	{'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
+	]	
+	is_male = {
+  	'Маша': False,
+  	'Оля': False,
+  	'Олег': True,
+  	'Миша': True,
+	}
+
+
+	for school_class in school:
+		boys = 0
+		girls = 0
+		for students in school_class['students']:
+			if is_male[students['first_name']]:
+				boys += 1
+			else:
+				girls += 1
+		print(f"В классе {school_class['class']} {girls} девочки and {boys} мальчика.")
+	
+
+
+
 
 # Пример вывода:
 # В классе 2a 2 девочки и 0 мальчика.
@@ -98,16 +114,39 @@ is_male = {
 
 # Задание 5
 # По информации о учениках разных классов нужно найти класс, в котором больше всего девочек и больше всего мальчиков.
-school = [
-  {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-  {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-]
-is_male = {
-  'Маша': False,
-  'Оля': False,
-  'Олег': True,
-  'Миша': True,
-}
+def count_5():	
+	school = [
+  	{'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
+  	{'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
+	]
+	is_male = {
+  	'Маша': False,
+  	'Оля': False,
+  	'Олег': True,
+  	'Миша': True,
+	}
+
+	total_gender_students = {}
+	for school_class in school:
+		boys = 0
+		girls = 0
+		for students in school_class['students']:
+			if is_male[students['first_name']]:
+				boys += 1
+			else:
+				girls += 1
+		total_gender_students[school_class['class']] = {'girls':girls, 'boys':boys}
+	print(total_gender_students)
+
+
+
+	
+	
+	
+		
+
+
+
 # ???
 
 # Пример вывода:
@@ -122,6 +161,10 @@ def main():
 	count_2()
 	print("*"*10)
 	count_3()
+	print("*"*10)
+	count_4()
+	print("*"*10)
+	
 
 
 
